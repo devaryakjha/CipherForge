0bbd70857aeb48933205df8fae33c4e519e2ce02
logiclynxAJ <dev.jha.arya@gmail.com>
2024-04-22 03:06:14 +0530

    Improved spacing for theme toggler

--- src/app/shared/components/header.tsx
+++ src/app/shared/components/header.tsx
@@ -2,7 +2,7 @@ import { ModeToggle } from "@/components/ui/theme-switcher";
 
 const Header = () => {
   return (
-    <header className="fixed top-8 left-8 right-8 flex items-center justify-end">
+    <header className="fixed top-2 right-2 left-2 sm:top-8 sm:left-8 sm:right-8 flex items-center justify-end">
       <ModeToggle />
     </header>
   );
