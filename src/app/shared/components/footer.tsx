0f523cc6731a32dbb0a09f72d915182dee1c9f4e
logiclynxAJ <dev.jha.arya@gmail.com>
2024-04-22 08:16:48 +0530

    Update Footer component styling in footer.tsx

--- src/app/shared/components/footer.tsx
+++ src/app/shared/components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="fixed bottom-8 left-0 right-0 flex items-center justify-center">
+    <footer className="fixed bottom-2 sm:bottom-8 left-0 right-0 flex items-center justify-center">
       <span>
         Made with{" "}
         <span role="img" aria-label="love">
@@ -11,7 +11,7 @@ const Footer = () => {
           href="https://aryak.dev"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-blue-500"
+          className="dark:text-blue-500 text-gray-500 hover:underline"
         >
           Aryakumar Jha
         </a>
