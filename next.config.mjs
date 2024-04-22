2d41277fe9f3320af09a4469be414b7e2e5fbc93
logiclynxAJ <dev.jha.arya@gmail.com>
2024-04-22 05:38:57 +0530

    Refactor next.config.mjs to use withBundleAnalyzer Remove unused imports and refactor components in src/components/ui/form.tsx Refactor Label component in src/components/ui/label.tsx

--- next.config.mjs
+++ next.config.mjs
@@ -3,4 +3,6 @@ import withBundleAnalyzer from "@next/bundle-analyzer";
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default process.env.ANALYZE === "true"
+  ? withBundleAnalyzer()(nextConfig)
+  : nextConfig;
--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import * as LabelPrimitive from "@radix-ui/react-label";
 
 import { cn } from "@/lib/utils";
 import { Label } from "@/components/ui/label";
@@ -13,8 +12,8 @@ const FormItem = React.forwardRef<
 FormItem.displayName = "FormItem";
 
 const FormLabel = React.forwardRef<
-  React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>
+  HTMLLabelElement,
+  React.LabelHTMLAttributes<HTMLLabelElement>
 >(({ className, ...props }, ref) => {
   return <Label ref={ref} className={className} {...props} />;
 });
